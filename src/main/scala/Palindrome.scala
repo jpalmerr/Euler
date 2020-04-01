16a9ac2e30d44ff784cefc3c1bf1389c013cf9ee
James Palmer <james.palmer@itv.com>
2020-04-01 17:23:39 +0100

    Largest palindrome

--- src/main/scala/Palindrome.scala
+++ src/main/scala/Palindrome.scala
@@ -1,3 +1,22 @@
-class Palindrome {
+/*
+A palindromic number reads the same both ways.
+The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+
+object Palindrome extends App {
+
+  def largestPalindrome(lowerBound: Int, upperBound: Int): Int = {
+    (lowerBound to upperBound).flatMap { i => // set range
+      (i to upperBound).map(_ * i) // multiply each number in range
+        .filter(predicate) // apply predicate
+    }.max
+  }
+
+  def predicate(n: Int): Boolean = n.toString == n.toString.reverse
+
+
+  println(largestPalindrome(100, 999)) // 906609
 
 }
--- src/test/scala/PalindromeSpec.scala
+++ src/test/scala/PalindromeSpec.scala
@@ -1,3 +1,12 @@
-class PalindromeSpec {
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class PalindromeSpec extends AnyWordSpec with Matchers {
+
+  "largestPalindrome" should {
+    "return largest palindrome" in {
+      Palindrome.largestPalindrome(10, 99) should equal(9009)
+    }
+  }
 
 }
