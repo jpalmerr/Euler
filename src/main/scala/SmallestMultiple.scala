95127fd16496600e4c2ecab3fea5110438e7714e
James Palmer <james.palmer@itv.com>
2020-04-01 17:24:14 +0100

    Smallest multiple

--- src/main/scala/SmallestMultiple.scala
+++ src/main/scala/SmallestMultiple.scala
@@ -1,3 +1,15 @@
-object SmallestMultiple {
+/*
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ */
+
+object SmallestMultiple extends App {
+
+    def smallestMultiple(x: Int): Int = {
+      val range = Range(x, Int.MaxValue)
+      range.find(n => Range(2, x + 1).forall(n % _ == 0)).get // ok to get as always returns a result
+    }
+
+    println(smallestMultiple(20)) // 232792560
 }
--- src/test/scala/SmallestMultipleSpec.scala
+++ src/test/scala/SmallestMultipleSpec.scala
@@ -1,3 +1,14 @@
-class SmallestMultipleSpec {
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
+class SmallestMultipleSpec extends AnyWordSpec with Matchers {
+
+  "smallestMultiple" should {
+    "return smallest multiple that is divisible by all between 1 and n" in {
+      SmallestMultiple.smallestMultiple(10) should equal(2520)
+      SmallestMultiple.smallestMultiple(3) should equal(6)
+      SmallestMultiple.smallestMultiple(4) should equal (12)
+      SmallestMultiple.smallestMultiple(1) should equal (1)
+    }
+  }
 }
